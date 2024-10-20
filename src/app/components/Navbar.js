6c3f893d379afa84c137e0c614057e277241c640
edsimonn <dami25edusi@gmail.com>
2024-10-20 19:00:02 +0100

    feat:

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdClose } from "react-icons/md";
 
@@ -16,22 +17,35 @@ const Navbar = () => {
     <nav className="w-full absolute top-0 left-0 z-50 flex items-center sm:justify-start justify-between px-6 py-3 text-primary max-w-screen ss:p-10 p-4 lg:py-8 lg:px-32 md:px-24">
       {/* Logo on the left */}
       <div className="flex items-center">
-        <img src="/images/nav-logo.png" alt="hero-logo" />
+        <Image
+          src="/images/nav-logo.png"
+          alt="hero-logo"
+          width={100}
+          height={50}
+        />
       </div>
 
       {/* Desktop menu */}
       <ul className="hidden sm:flex space-x-10 ml-2 lg:ml-24 flex-wrap">
         <li>
-          <Link href="/" className="font-bold text-xl">Home</Link>
+          <Link href="/" className="font-bold text-xl">
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/" className="font-normal text-xl">About</Link>
+          <Link href="/" className="font-normal text-xl">
+            About
+          </Link>
         </li>
         <li>
-          <Link href="/shop" className="font-normal text-xl">Shop</Link>
+          <Link href="/shop" className="font-normal text-xl">
+            Shop
+          </Link>
         </li>
         <li>
-          <Link href="/faqs" className="font-normal text-xl">FAQs</Link>
+          <Link href="/faqs" className="font-normal text-xl">
+            FAQs
+          </Link>
         </li>
       </ul>
 
@@ -39,9 +53,13 @@ const Navbar = () => {
       <div className="block sm:hidden relative text-primary z-50">
         <button className="focus:outline-none" onClick={toggleMenu}>
           {isOpen ? (
-            <span className="text-2xl"><MdClose /></span> // Close button
+            <span className="text-2xl">
+              <MdClose />
+            </span> // Close button
           ) : (
-            <span className="text-2xl"><RxHamburgerMenu /></span> // Open button
+            <span className="text-2xl">
+              <RxHamburgerMenu />
+            </span> // Open button
           )}
         </button>
 
